f8fc025de261c62fa6a7a0f9c438455542ef7772
Steven Talbot <steven@pelotoncycle.com>
2016-02-29 18:17:44 -0800

    Upgrade project to 1.8

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.priority-map "0.0.7"]
                  [org.clojure/data.json "0.2.6"]])
