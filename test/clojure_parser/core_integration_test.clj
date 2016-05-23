0f3af8b66a62fc84b435b47915612eaad90dd1a8
Steven Talbot <steven@pelotoncycle.com>
2016-05-23 08:53:51 -0700

    Added in more semantics to integration test

--- test/clojure_parser/core_integration_test.clj
+++ test/clojure_parser/core_integration_test.clj
@@ -7,20 +7,31 @@
 
 (def super-realistic-pcfg
   {
-   "$S" {:productions [{:elements ["$NP" "$VP"], :count 4}
-                       {:elements ["$NP" "$VP"], :count 4}]
+   "$S" {:productions [{:elements ["$NP" "$VP"], :count 4, :sem ["#&1" "%0"]}]
          :isolate_features ["plural" "person"]}
-   "$NP" {:productions [{:elements ["$AP" "$N"], :count 0.3}
-                        {:elements ["$N" "$N"], :count 0.3}
-                        {:elements ["$N"], :count 0.7}]}
-   "$AP" {:productions [{:elements ["$RP" "$AA"], :count 0.1}
-                        {:elements ["$A"], :count 0.4}]}
-   "$AA" {:productions [{:elements ["$A" "$AA"], :count 0.3}
+   "$NP" {:productions [{:elements ["$NN"], :count 0.6}
+                        {:elements ["$AP" "$NN"], :sem ["&0" "&1"], :count 0.4}
+                        {:elements ["$NP" "$C" "$NP"],
+                         :count 0.1,
+                         :sem ["&#1" "%0" "%2"]}]}
+   "$NN" {:productions [{:elements ["$N" "$N"], :count 0.1, :sem ["&0" "&1"]}
+                        {:elements ["$N"], :count 0.6}]}
+   "$AP" {:productions [{:elements ["$RP" "$AA"], :count 0.1, :sem ["&1"]}
+                        {:elements ["$AA"], :count 0.4}]}
+   "$AA" {:productions [{:elements ["$A" "$AA"], :count 0.3, :sem ["&0" "&1"]}
                         {:elements ["$A"], :count 0.5}]}
    "$RP" {:productions [{:elements ["$R" "$RP"], :count 0.7}
                         {:elements ["$R"], :count 0.7}]}
-   "$VP" {:productions [{:elements ["$V" "$NP"], :count 0.4, :head 0}
-                        {:elements ["$V"], :count 0.6}]}
+   "$VP" {:productions [{:elements [["$V" {:trans true}] "$NP"],
+                         :count 0.4,
+                         :sem ["#&0" "@0" "%1"],
+                         :head 0}
+                        {:elements [["$V" {:trans false}]],
+                         :sem ["&#0" "@0"]
+                         :count 0.6}]}
+   :meta {:sem-mapper {"$V" {:key "trans", :vals {:true ["#&0" "@0" "%1"],
+                                                  :nil ["&#0" "@0"]}},
+                       "$C" ["&#1" "%0" "%2"]}}
    })
 
 (defn kind-of-realistic-lexicon1 []
@@ -49,6 +60,9 @@
       (clojure.string/split (clojure.string/lower-case sentence), #" ")
       beam-size
       false)))
+
+(deftest some-sentence
+  (quick-parse "cows eat green grass" 50))
 
 
 
