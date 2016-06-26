f7bffa5156dfa0f535e1289be0c382b531e24828
Steven Talbot <steven@pelotoncycle.com>
2016-06-26 11:48:42 -0700

    Added a pcfg-container in the application code so the main part of the application can get access to the wordnet-based pcfg/lex

---
+++ src/clojure_parser/pcfg_container.clj
@@ -0,0 +1,54 @@
+(ns clojure-parser.pcfg-container
+  (:require [clojure-parser.pcfg-compiler :refer :all]))
+
+(def super-realistic-pcfg
+  {
+   "$S" {:productions [{:elements ["$DP" "$VP"], :count 4, :sem ["#&1" "%0"], :full_features ["plural" "person"]}]
+         :isolate_features [ "person"]}
+   "$NP" {:productions [{:elements ["$NN"], :count 0.6}
+                        {:elements ["$AP" "$NN"], :sem ["&0" "&1"], :count 0.4}]}
+   "$DP" {:productions [{:elements ["$D" "$NP"], :count 0.6, :sem ["&0" "&1"]}
+                        {:elements ["$NP"], :count 0.4}
+                        {:elements [["$VP" {:tense "present_part"}]], :count 0.2}
+                        {:elements ["$DP" "$PP"], :count 0.2, :sem ["#1" "&%0"], :head 0}
+                        {:elements ["$DP" "$C" "$DP"],
+                         :count 0.1,
+                         :sem ["&#1" "%0" "%2"]}]}
+   "$NN" {:productions [{:elements [["$N" {:plural false}] "$N"], :count 0.1, :sem ["&0" "&1"]}
+                        {:elements ["$N"], :count 0.6}]}
+   "$AP" {:productions [{:elements ["$RP" "$AA"], :count 0.1, :sem ["^#0" "^%&1"]}
+                        {:elements ["$AA"], :count 0.4}
+                        {:elements ["$AP" "$C" "$AP"], :count 0.1, :sem ["&0" "&2"]}]}
+   "$AA" {:productions [{:elements ["$A" "$AA"], :count 0.3, :sem ["&0" "&1"]}
+                        {:elements ["$A"], :count 0.5}]}
+   "$RP" {:productions [{:elements ["$R" "$RP"], :count 0.7, :sem ["&0" "&1"]}
+                        {:elements ["$R"], :count 0.7}]}
+   "$VP" {:productions [{:elements ["$VP" "$PP"] :count 0.75, :sem ["#1" "&%0"], :head 0}
+                        {:elements ["$VV"] :count 0.75}
+                        {:elements ["$VV" "$RP"] :count 0.25, :sem ["&0" "&1"], :head 0}]}
+   "$PP" {:productions [{:elements ["$P" "$DP"] :count 1.0 :sem ["&^#0" "@0" "%1"]}]}
+   "$VV" {:productions [{:elements [["$V" {:trans true}] "$DP"],
+                         :count 0.4,
+                         :sem ["#&0" "@0" "%1"],
+                         :head 0}
+                        {:elements [["$V" {:trans false}]],
+                         :sem ["&#0" "@0"]
+                         :count 0.6}
+                        {:elements [["$V" {:trans "ditrans"}] "$DP" "$DP"],
+                         :sem ["&#0" "@0" "%1" "%2"]
+                         :count 0.1}]} ; TODO: ditrans with to!
+   })
+
+(defn global-lexicon []
+  (load-lex-from-wn-path "resources/wordnet_as_json"))
+
+(defn global-pcfg-and-lex' []
+  (println "Building large lexicon and pcfg, please be patient...")
+  (let [raw-lex (global-lexicon)
+        result [(make-lexical-lkup raw-lex)
+                (build-operational-pcfg (lexicalize-pcfg super-realistic-pcfg raw-lex))]]
+    (println "finished building result of sizes " (mapv count result))
+    result)
+  )
+
+(def global-pcfg-and-lex (memoize global-pcfg-and-lex'))
--- test/clojure_parser/core_integration_test.clj
+++ test/clojure_parser/core_integration_test.clj
@@ -3,64 +3,14 @@
             [clojure-parser.core :refer :all]
             [clojure-parser.pcfg-compiler :refer :all]
             [clojure-parser.utils :refer :all]
+            [clojure-parser.pcfg-container :refer [global-pcfg-and-lex]]
             [clojure.zip :as zp]
             [clojure.data :refer [diff]]
             ))
 
-(def super-realistic-pcfg
-  {
-   "$S" {:productions [{:elements ["$DP" "$VP"], :count 4, :sem ["#&1" "%0"], :full_features ["plural" "person"]}]
-         :isolate_features [ "person"]}
-   "$NP" {:productions [{:elements ["$NN"], :count 0.6}
-                        {:elements ["$AP" "$NN"], :sem ["&0" "&1"], :count 0.4}]}
-   "$DP" {:productions [{:elements ["$D" "$NP"], :count 0.6, :sem ["&0" "&1"]}
-                        {:elements ["$NP"], :count 0.4}
-                        {:elements [["$VP" {:tense "present_part"}]], :count 0.2}
-                        {:elements ["$DP" "$PP"], :count 0.2, :sem ["#1" "&%0"], :head 0}
-                        {:elements ["$DP" "$C" "$DP"],
-                         :count 0.1,
-                         :sem ["&#1" "%0" "%2"]}]}
-   "$NN" {:productions [{:elements [["$N" {:plural false}] "$N"], :count 0.1, :sem ["&0" "&1"]}
-                        {:elements ["$N"], :count 0.6}]}
-   "$AP" {:productions [{:elements ["$RP" "$AA"], :count 0.1, :sem ["^#0" "^%&1"]}
-                        {:elements ["$AA"], :count 0.4}
-                        {:elements ["$AP" "$C" "$AP"], :count 0.1, :sem ["&0" "&2"]}]}
-   "$AA" {:productions [{:elements ["$A" "$AA"], :count 0.3, :sem ["&0" "&1"]}
-                        {:elements ["$A"], :count 0.5}]}
-   "$RP" {:productions [{:elements ["$R" "$RP"], :count 0.7, :sem ["&0" "&1"]}
-                        {:elements ["$R"], :count 0.7}]}
-   "$VP" {:productions [{:elements ["$VP" "$PP"] :count 0.75, :sem ["#1" "&%0"], :head 0}
-                        {:elements ["$VV"] :count 0.75}
-                        {:elements ["$VV" "$RP"] :count 0.25, :sem ["&0" "&1"], :head 0}]}
-   "$PP" {:productions [{:elements ["$P" "$DP"] :count 1.0 :sem ["&^#0" "@0" "%1"]}]}
-   "$VV" {:productions [{:elements [["$V" {:trans true}] "$DP"],
-                         :count 0.4,
-                         :sem ["#&0" "@0" "%1"],
-                         :head 0}
-                        {:elements [["$V" {:trans false}]],
-                         :sem ["&#0" "@0"]
-                         :count 0.6}
-                        {:elements [["$V" {:trans "ditrans"}] "$DP" "$DP"],
-                         :sem ["&#0" "@0" "%1" "%2"]
-                         :count 0.1}]} ; TODO: ditrans with to!
-   })
 
-(defn kind-of-realistic-lexicon1 []
-  (load-lex-from-wn-path "resources/wordnet_as_json"))
-
-(defn pcfg-and-lex-for-test1 []
-  (println "Building large lexicon and pcfg, please be patient...")
-  (let [raw-lex (kind-of-realistic-lexicon1)
-        result [(make-lexical-lkup raw-lex)
-                (build-operational-pcfg (lexicalize-pcfg super-realistic-pcfg raw-lex))]]
-    (println "finished building result of sizes " (mapv count result))
-    result)
-  )
-
-(def pcfg-and-lex-for-test (memoize pcfg-and-lex-for-test1))
-
-(defn lex-for-test [] (first (pcfg-and-lex-for-test)))
-(defn pcfg-for-test [] (second (pcfg-and-lex-for-test)))
+(defn lex-for-test [] (first (global-pcfg-and-lex)))
+(defn pcfg-for-test [] (second (global-pcfg-and-lex)))
 
 (deftest dont-blow-up-building-large-structures
   (is (not= (lex-for-test) (pcfg-for-test))))
